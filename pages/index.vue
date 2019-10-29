913c6c628eeaf9371900746fd2b856ce8a37f675
Tomohiro <43288418+nztm@users.noreply.github.com>
2019-10-30 00:01:48 +0900

    Facebook URLを変更

--- pages/index.vue
+++ pages/index.vue
@@ -8,13 +8,13 @@ html
         p.job Front-end Engineer / Designer
         .skill
           p Vue.js / Nuxt.js / Expo
-          p Motion Animation / Web Design
+          p Motion Amation / Web Design
           p Photograph / Tech Blogger
         .btn
           a.tw-btn(href="https://twitter.com/nztm_tw")
             i.fab.fa-twitter
           a.insta-btn(href="https://www.instagram.com/nztm_insta")
             i.fab.fa-instagram
-          a.fb-btn(href="https://www.facebook.com/profile.php?id=100012512234101")
+          a.fb-btn(href="https://www.facebook.com/nztm.fcb")
             i.fab.fa-facebook-f
 </template>
